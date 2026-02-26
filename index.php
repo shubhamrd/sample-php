6548dfdeb488ab46fae08e466e3518722c9bafa3
Shubham Dalvi <shubhamrdalvi@yahoo.in>
2026-02-26 09:05:38 +0530

    Update title in index.php to include full name

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shubham - Cloud Technical Consultant & Solution Architect</title>
+    <title>Shubham Dalvi - Cloud Technical Consultant & Solution Architect </title>
     <!-- Load Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
